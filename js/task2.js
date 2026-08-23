a6eda8d96417e01d00d344b4fe2a2d60bd97787c
malak8204 <malakmahmoud685@gmail.com>
2026-08-24 01:03:47 +0300

    Add files via upload

---
+++ js/task2.js
@@ -0,0 +1,17 @@
+var grade=window.prompt();
+if(grade>=90){
+    console.log('Your grade is Excellent');
+    
+}else if(80<grade<89){
+    console.log('your grade is Good');
+    
+}else if(70<grade<79){
+    console.log('Your grade is Average');
+    
+}else if(60<grade<69){
+    console.log('Your grade is Pass');
+    
+}else{
+    console.log('You failed');
+    
+}
