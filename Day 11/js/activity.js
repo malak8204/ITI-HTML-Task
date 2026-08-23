2595ed43044adebd8bb8e630f907ad58101292bd
malak8204 <malakmahmoud685@gmail.com>
2026-08-24 01:08:14 +0300

    Add number classification logic in activity.js

---
+++ Day 11/js/activity.js
@@ -0,0 +1,17 @@
+var num = -8
+if (num%2==0){
+   console.log('the number is even');
+    
+}else{
+console.log('Number is odd');
+
+} if(num==0){
+    console.log('Number is zero' );
+    
+}else if(num>0){
+    console.log('Number is Positive');
+    
+}else if (num<0){
+    console.log('Number is Negative');
+    
+}
